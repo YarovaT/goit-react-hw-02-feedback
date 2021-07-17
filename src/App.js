cc06ea132b9385b8325a8c60741ae283c1c32f46
Tetiana Yarova <andronikova11@gmail.com>
2021-07-17 17:04:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import Statistics from "./components/Statistics/Statistics";
-import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import React, { Component } from 'react';
+import './App.css';
+import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Notification from './components/Notification/Notification';
 
-function App() {
-  return (
-    <div className="App">
-      <FeedbackOptions />
-      <Statistics />
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((a, b) => a + b, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+
+    let positivePercentage;
+
+    if (good > 0) {
+      positivePercentage = (good / total) * 100;
+    } else positivePercentage = 0;
+
+    return Math.round(positivePercentage);
+  };
+
+  onLeaveFeedback = event => {
+    const name = event.target.name;
+
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({
+      visible: !prevState.visible,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const objKey = Object.keys(this.state);
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={objKey}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </>
+    );
+  }
 }
 
 export default App;
