94def7ab801bc0c73ca673f0d84ca48b1cfcb19a
Tetiana Yarova <andronikova11@gmail.com>
2021-07-17 17:02:25 +0300

    Update Statistic component

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,61 +1,38 @@
-import React from "react";
-// import PropTypes from "prop-types";
-import Section from "../Section/Section";
-import s from "./Statistics.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
-class Statistics extends React.Component {
-  // constructor() {
-  //   super();
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <ul className={s.statisticsList}>
+    <li className={s.statisticsItem}>
+      {' '}
+      Good: <span>{good}</span>
+    </li>
+    <li className={s.statisticsItem}>
+      {' '}
+      Neutral: <span>{neutral}</span>
+    </li>
+    <li className={s.statisticsItem}>
+      {' '}
+      Bad: <span>{bad}</span>
+    </li>
+    <li className={s.statisticsItem}>
+      {' '}
+      Total: <span>{total}</span>
+    </li>
+    <li className={s.statisticsItem}>
+      {' '}
+      PositivePercentag: <span>{positivePercentage}%</span>
+    </li>
+  </ul>
+);
 
-  //   this.state = {
-  //     good: 5,
-  //     neutral: 0,
-  //     bad: 0,
-  //   };
-  // }
-
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  render() {
-    return (
-      <Section title="Statistics">
-        <ul className={s.statisticsList}>
-          <li className={s.statisticsItem}>
-            {" "}
-            Good: <span>{this.state.good}</span>
-          </li>
-          <li className={s.statisticsItem}>
-            {" "}
-            Neutral: <span>{this.state.neutral}</span>
-          </li>
-          <li className={s.statisticsItem}>
-            {" "}
-            Bad: <span>{this.state.bad}</span>
-          </li>
-          <li className={s.statisticsItem}>
-            {" "}
-            Total: <span>0</span>
-          </li>
-          <li className={s.statisticsItem}>
-            {" "}
-            PositivePercentag: <span>0</span>
-          </li>
-        </ul>
-      </Section>
-    );
-  }
-}
-
-// Statistics.propTypes = {
-//   state: PropTypes.shape({
-//     good: PropTypes.number.isRequired,
-//     neutral: PropTypes.number.isRequired,
-//     bad: PropTypes.number.isRequired,
-//   }),
-// };
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
 
 export default Statistics;
